d15e5b2d098eb38e7b9155a5b5b69c42b9c1902c
HarithaRugada <haritharugada@gmail.com>
2020-06-21 12:38:50 +0530

    ADDED : Generic way to find the maximum of the elements

--- src/main/java/com/findmaximum/FindMaximum.java
+++ src/main/java/com/findmaximum/FindMaximum.java
@@ -1,48 +1,18 @@
 package com.findmaximum;
 
-import java.awt.font.ShapeGraphicAttribute;
-
 public class FindMaximum
 {
-    public Integer getMaximumInteger(Integer firstNumber,Integer secondNumber,Integer thirdNumber)
+    public static <E extends Comparable<E>> E getMaximum(E first,E second,E third)
     {
-        Integer maximumInteger=firstNumber;
-        if(secondNumber.compareTo(maximumInteger) > 0)
+        E maximum=first;
+        if(second.compareTo(maximum) > 0)
         {
-            maximumInteger=secondNumber;
+            maximum=second;
         }
-        if(thirdNumber.compareTo(maximumInteger) > 0)
+        if(third.compareTo(maximum) > 0)
         {
-            maximumInteger=thirdNumber;
+            maximum=third;
         }
-        return maximumInteger;
-    }
-
-    public double getMaximumDouble(Double firstNumber,Double secondNumber,Double thirdNumber)
-    {
-        Double maximumDouble=firstNumber;
-        if(secondNumber.compareTo(maximumDouble) > 0)
-        {
-            maximumDouble=secondNumber;
-        }
-        if(thirdNumber.compareTo(maximumDouble) > 0)
-        {
-            maximumDouble=thirdNumber;
-        }
-        return maximumDouble;
-    }
-
-    public String getMaximumString(String firstString,String secondString,String thirdString)
-    {
-        String maximumString=firstString;
-        if(secondString.compareTo(maximumString) > 0)
-        {
-            maximumString=secondString;
-        }
-        if(thirdString.compareTo(maximumString) > 0)
-        {
-            maximumString=thirdString;
-        }
-        return maximumString;
+        return maximum;
     }
 }
--- src/test/java/com/findmaximum/FindMaximumTest.java
+++ src/test/java/com/findmaximum/FindMaximumTest.java
@@ -10,54 +10,54 @@ public class FindMaximumTest
     @Test
     public void maxIntegerNumber_atFirstPosition_returnTheNumber()
     {
-        Assert.assertEquals(86,(int)findMaximum.getMaximumInteger(86,34,78));
+        Assert.assertEquals(86,(int)findMaximum.getMaximum(86,34,78));
     }
 
     @Test
     public void maxIntegerNumber_atSecondPosition_returnTheNumber()
     {
-        Assert.assertEquals(72,(int)findMaximum.getMaximumInteger(65,72,48));
+        Assert.assertEquals(72,(int)findMaximum.getMaximum(65,72,48));
     }
 
     @Test
     public void maxIntegerNumber_atThirdPosition_returnTheNumber()
     {
-        Assert.assertEquals(34,(int)findMaximum.getMaximumInteger(31,12,34));
+        Assert.assertEquals(34,(int)findMaximum.getMaximum(31,12,34));
     }
 
     @Test
     public void maxDoubleNumber_atFirstPosition_returnTheNumber()
     {
-        Assert.assertEquals((Double)86.6,(Double)findMaximum.getMaximumDouble(86.6,34.8,78.7));
+        Assert.assertEquals((Double)86.6,(Double)findMaximum.getMaximum(86.6,34.8,78.7));
     }
 
     @Test
     public void maxDoubleNumber_atSecondPosition_returnTheNumber()
     {
-        Assert.assertEquals((Double)72.54,(Double) findMaximum.getMaximumDouble(65.93,72.54,48.75));
+        Assert.assertEquals((Double)72.54,(Double) findMaximum.getMaximum(65.93,72.54,48.75));
     }
 
     @Test
     public void maxDoubleNumber_atThirdPosition_returnTheNumber()
     {
-        Assert.assertEquals((Double)34.12,(Double) findMaximum.getMaximumDouble(31.90,12.98,34.12));
+        Assert.assertEquals((Double)34.12,(Double) findMaximum.getMaximum(31.90,12.98,34.12));
     }
 
     @Test
     public void maxString_atFirstPosition_returnTheString()
     {
-        Assert.assertEquals((String)"banana",(String)findMaximum.getMaximumString("banana", "air", "apple"));
+        Assert.assertEquals((String)"banana",(String)findMaximum.getMaximum("banana", "air", "apple"));
     }
 
     @Test
     public void maxString_atSecondPosition_returnTheString()
     {
-        Assert.assertEquals((String)"elephant",(String)findMaximum.getMaximumString("cat", "elephant", "bird"));
+        Assert.assertEquals((String)"elephant",(String)findMaximum.getMaximum("cat", "elephant", "bird"));
     }
 
     @Test
     public void maxString_atThirdPosition_returnTheString()
     {
-        Assert.assertEquals((String)"orchid",(String)findMaximum.getMaximumString("daisy", "iris", "orchid"));
+        Assert.assertEquals((String)"orchid",(String)findMaximum.getMaximum("daisy", "iris", "orchid"));
     }
 }
