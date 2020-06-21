c201893bc9e1638106858bd649d04df16004fded
HarithaRugada <haritharugada@gmail.com>
2020-06-21 10:31:57 +0530

    ADDED : Find the maximum of three integers

--- src/main/java/com/findmaximum/FindMaximum.java
+++ src/main/java/com/findmaximum/FindMaximum.java
@@ -1,4 +1,18 @@
 package com.findmaximum;
 
-public class FindMaximum {
+public class FindMaximum
+{
+    public Integer getMaximum(Integer firstNumber,Integer secondNumber,Integer thirdNumber)
+    {
+        Integer maximumInteger=firstNumber;
+        if(secondNumber.compareTo(maximumInteger) > 0)
+        {
+            maximumInteger=secondNumber;
+        }
+        if(thirdNumber.compareTo(maximumInteger) > 0)
+        {
+            maximumInteger=thirdNumber;
+        }
+        return maximumInteger;
+    }
 }
--- src/test/java/com/findmaximum/FindMaximumTest.java
+++ src/test/java/com/findmaximum/FindMaximumTest.java
@@ -1,4 +1,27 @@
 package com.findmaximum;
 
-public class FindMaximumTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindMaximumTest
+{
+    FindMaximum findMaximum=new FindMaximum();
+
+    @Test
+    public void maxNumber_atFirstPosition_returnTheNumber()
+    {
+        Assert.assertEquals(86,(int)findMaximum.getMaximum(86,34,78));
+    }
+
+    @Test
+    public void maxNumber_atSecondPosition_returnTheNumber()
+    {
+        Assert.assertEquals(72,(int)findMaximum.getMaximum(65,72,48));
+    }
+
+    @Test
+    public void maxNumber_atThirdPosition_returnTheNumber()
+    {
+        Assert.assertEquals(34,(int)findMaximum.getMaximum(31,12,34));
+    }
 }
